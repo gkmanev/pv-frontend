481be6ed36b9567e4d69d59211f448eccc6bba1d
Georgi <georgi.manev@entra.energy>
2024-03-07 12:13:48 +0200

    up

--- src/views/dashboards/dashboard-components/echarts/LineChart.vue
+++ src/views/dashboards/dashboard-components/echarts/LineChart.vue
@@ -1,7 +1,7 @@
 <template>
   <b-card class="mb-4">
     <div class="mt-4">
-      <v-chart class="chart" height="450" :option="option" @touchstart="getDataSubset" autoresize/>
+      <v-chart class="chart" height="450" :option="option" @mouseover="getDataSubset" autoresize/>
     </div>
   </b-card>
 </template>
@@ -50,7 +50,7 @@ var timeLineSet = function(value) {
   return `${hours}:${minutes}`;
 }
 
-var tooltipDisplay = 'xyz'
+var tooltipDisplay = ''
 
 export default {
   name: "HelloWorld",
